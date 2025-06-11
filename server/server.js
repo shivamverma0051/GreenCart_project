d6bd092ba8507ba233f17728f44d5817fbf68d7c
shivam <vermashivam0051@gmail.com>
2025-06-11 18:30:22 +0530

    up

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 4000;
 await connectDB(); // ✅ Only DB connection, no cloudinary needed
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://greencart-project-frontend.onrender.com/'];
+const allowedOrigins = ['http://localhost:5173'];
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
